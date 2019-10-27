b1fbb47576f9eb7c71c17a955d0ffa30d7c63975
易拉罐tony <m18458855501@163.com>
2019-10-27 21:26:35 +0800

    发现页面开发

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2019-10-27 10:58:18
+ * @LastEditTime: 2019-10-27 11:17:12
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /devProject/src/App.vue
+ -->
 <template>
   <div id="app">
     <div class="view-container">
@@ -26,6 +34,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  height: 100vh;
   .buttom-menu {
     height: 64px;
   }
---
+++ src/views/discover/index.vue
@@ -0,0 +1,100 @@
+<!--
+ * @Author: your name
+ * @Date: 2019-10-22 22:12:01
+ * @LastEditTime: 2019-10-27 21:23:19
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /devProject/src/views/discover/discover.vue
+ -->
+<template>
+  <div class="box-warpper">
+    <van-nav-bar title="发现" v-if="false" />
+    <div>
+      <list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <div>
+          <div
+            v-for="(item,index) in list"
+            :key="index"
+            class="discover-list"
+            @click="goDetail(item)"
+          >
+            <div class="title">{{item.title}}</div>
+            <div class="date">{{item.date}}</div>
+          </div>
+        </div>
+      </list>
+    </div>
+  </div>
+</template>
+<script>
+import { NavBar, List } from 'vant'
+export default {
+  components: {
+    [NavBar.name]: NavBar,
+    List
+  },
+  data: function () {
+    return {
+      loading: false,
+      finished: false,
+      list: [
+
+      ]
+    }
+  },
+  methods: {
+    onLoad () {
+      let arr = [
+        {
+          title: '新闻标题',
+          date: '2019-10-21 19:00'
+        }
+      ]
+      setTimeout(() => {
+        this.list = arr
+        for (let i = 0; i < 10; i++) {
+          this.list.push({
+            title: '新闻标题',
+            date: '2019-10-21 19:00'
+          })
+        }
+        this.loading = false
+
+        if (this.list.length >= 11) {
+          this.finished = true
+        }
+      }, 100)
+      console.log(3)
+    },
+    goDetail (item) {
+      this.$router.push({
+        name: 'richPage',
+        query: {
+          title: '养身'
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.box-warpper {
+  height: 100vh;
+  font-size: 14px;
+  background: #fff;
+  .discover-list {
+    border-top: 1px solid #d6d4d4;
+    height: 30px;
+    padding: 10px;
+    .title {
+      padding-bottom: 4px;
+    }
+    .date {
+      font-size: 12px;
+    }
+  }
+  .discover-list:last-child {
+    border-bottom: 1px solid #d6d4d4;
+  }
+}
+</style>
