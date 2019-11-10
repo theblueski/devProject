42fd932052c90ef909550601651ee71a4deab715
易拉罐tony <m18458855501@163.com>
2019-11-10 20:51:31 +0800

    显示微信头像

--- src/utils/globalFn.js
+++ src/utils/globalFn.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-11-10 15:52:07
- * @LastEditTime: 2019-11-10 16:29:23
+ * @LastEditTime: 2019-11-10 20:26:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /devProject/src/utils/globalFn.js
@@ -10,4 +10,10 @@ import store from '@/store'
 window.setBluetoothList = function (data) {
   store.commit('bluetooth/SETBLUETOOTHLIST', data)
   console.log(store.state, 'store')
+}
+
+window.getCookie = function (name) {
+  var arr; var reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)')
+  // eslint-disable-next-line no-cond-assign
+  if (arr = document.cookie.match(reg)) { return unescape(arr[2]) } else { return null }
 }
--- src/views/mine/index.vue
+++ src/views/mine/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2019-10-27 21:31:05
- * @LastEditTime: 2019-10-27 22:13:08
+ * @LastEditTime: 2019-11-10 20:51:09
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /devProject/src/views/mine/index.vue
@@ -9,8 +9,13 @@
 <template>
   <section class="box-wrapper">
     <div class="user-info">
-      <div class="head-portrait">头像</div>
-      <div class="user-name">张三</div>
+      <div class="head-portrait">
+      <van-image
+        class='image-style'
+        :src='imageUrl'
+        />
+      </div>
+      <div class="user-name">{{nickname}}</div>
     </div>
     <div class="list">
       <div class="mine-device" @click="goDevice">我的设备</div>
@@ -19,7 +24,24 @@
   </section>
 </template>
 <script>
+import { Image } from 'vant'
 export default {
+  data: function () {
+    return {
+      imageUrl: '',
+      nickname: ''
+    }
+  },
+  components: {
+    [Image.name]: Image
+  },
+  created () {
+    // window.document.cookie = 'headimgurl=http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/0'
+    // window.document.cookie = 'nickname=nihao'
+    this.imageUrl = window.getCookie('headimgurl')
+    this.nickname = window.getCookie('nickname')
+    console.log(this.imageUrl)
+  },
   methods: {
     goDevice () {
       this.$router.push({
@@ -52,6 +74,12 @@ export default {
       text-align: center;
       line-height: 60px;
       margin-right: 20px;
+      .image-style{
+          width:100%;
+          height:100%;
+          border-radius:50%;
+          overflow:hidden;
+      }
     }
   }
   .list {
